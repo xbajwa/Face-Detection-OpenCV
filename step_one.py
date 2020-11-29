a22a2f276b798c5de2932272832fe219f83d419b
mekditibe <49551563+mekditibe@users.noreply.github.com>
2020-11-29 15:50:36 +0100

    Add files via upload

--- step_one.py
+++ step_one.py
@@ -4,6 +4,9 @@ from typing import List
 import cv2
 import os
 
+from cv2.cv2 import imshow
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
 class VideoClass:
     def __init__(self, name, vid):
         self.name = name
@@ -13,11 +16,18 @@ class VideoClass:
         while self.video.isOpened():
             ret, frame = self.video.read()
             if ret:
+                faces = face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                for (column, row, width, height) in faces:
+                    cv2.rectangle(frame, (column, row), (column + width, row + height), (0, 255, 0), 2)
                 cv2.imshow(self.name, frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                key = cv2.waitKey(0)
+                while key not in [ord('q'), ord('k')]:
+                    key = cv2.waitKey(0)
+                if key == ord('q'):
                     break
         self.video.release()
         cv2.destroyAllWindows()
+
 
 videos: List[VideoClass] = []
 
@@ -34,14 +44,3 @@ print(len(videos))
 print(videos[0].video)
 VideoClass.videoShow(videos[0])
 
-# vid = cv2.VideoCapture('C:/Users/DELL/Desktop/week 1-5/Visual Data/VIRAT Ground Dataset/videos_original/VIRAT_S_000002.mp4')
-#while vid.isOpened():
- #   ret, frame = vid.read()
-  #  if ret:
-   #     cv2.imshow('Frame', frame)
-    #    if cv2.waitKey(25) & 0xFF == ord('q'):
-     #       break
-    #else:
-     #   break
-#vid.release()
-#cv2.destroyAllWindows()
