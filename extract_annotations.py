b4b47b626cc144183c6ad9cf941f9716e6ef5ae1
Awais <66957102+xbajwa@users.noreply.github.com>
2020-12-21 00:20:34 +0100

    fixed human annotations filtering

--- extract_annotations.py
+++ extract_annotations.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 for i in range(0,315):
-    df = pd.read_csv(f'C:/Users/opera_user/Documents/folder/{i}.txt', delimiter=' ', header=None,error_bad_lines=False) # reading object annotation files given in VIRAT Dataset
+    annot = pd.read_csv(f'C:/Users/opera_user/Documents/folder/{i}.txt', delimiter=' ', header=None,error_bad_lines=False) # reading object annotation files given in VIRAT Dataset
+    df = annot[annot[7] == 1] # filtering out annotations only for humans
     df = df.drop(columns = [0, 1, 7]) # Dropping unnecessary columns
     df = df.sort_values(by=2) # Sorting by frame no.
     df.insert(1, value=1, column='t') # insert number of objects to use it for opencv training
