188764b118f77eb743dce7117768a46a50abc4e3
mekditibe <49551563+mekditibe@users.noreply.github.com>
2021-01-30 20:24:40 +0100

    Add files via upload

--- videoBlur.py
+++ videoBlur.py
@@ -18,7 +18,6 @@ class VideoClass:
         i = 0
         while self.video.isOpened():
             ret, frame = self.video.read()
-
             if ret:
                 nf = self.video.get(cv2.CAP_PROP_POS_FRAMES)
                 if nf > 2:
@@ -30,10 +29,23 @@ class VideoClass:
                                 break
                             column = round(ann.values[i, 1])
                             row = round(ann.values[i, 2])
-                            width = round(ann.values[i, 3] - 15)
-                            height = round(ann.values[i, 4] - 112)
+                            width = round(ann.values[i, 3] )#- 12) # - 12
+                            height = round(ann.values[i, 4] )#- 112) # - 112
+                            kernel_width = (width // 7) | 1
+                            kernel_height = (height // 7) | 1
+                            if column % 2 == 0:
+                                column = column + 1
+                            if row % 2 == 0:
+                                row = row + 1
+                            cv2.rectangle(frame, (column, row), (column + width - 12, row + height - 112), (0, 255, 255), 1)
+                            face_color = frame[row:row + height, column:column + width]
+                            #blur = cv2.medianBlur(face_color, 9)
+                            #blur = cv2.bilateralFilter(face_color, 51, 75, 75)
+                            #blur = cv2.GaussianBlur(face_color, (13,13),0)
+                            blur = cv2.GaussianBlur(face_color, (kernel_width, kernel_height), 0)
+
+                            frame[row:row + height, column:column + width] = blur
                             i = i + 1
-                            cv2.rectangle(frame, (column, row), (column + width, row + height), (0, 255, 255), 1)
                     cv2.imshow(self.name, frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                          break
